9669f37a11852ef4a0b3a0a2b8ea5fb4f7696885
zeimedee <42755623+zeimedee@users.noreply.github.com>
2025-04-30 14:42:33 +0000

    database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 
+	_ "github.com/lib/pq"
 	"log"
 )
 
@@ -22,6 +25,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Define  an application struck to hold the dependencies for our http handlers, helpers and middleware.
@@ -40,11 +46,23 @@ func main() {
 	//default to using port number 4000 and environment "development" if no corresponding flags are provided
 	flag.IntVar(&cfg.port, "port", 4000, "API SERVER PORT")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:password@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	//Initialize a new logger  which writes messages to the standard out stream,
 	//prefixed with the current date and time.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	//Declare an instance of the application struct, containing the config struct and the logger
 
@@ -65,6 +83,23 @@ func main() {
 
 	//Start the http server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/zeimedee/greenlight/internal/data"
+	"github.com/zeimedee/greenlight/internal/validator"
 )
 
 //Add a createMovieHandler for the "POST /v1/movies" endpoint. for now we simply returna plain-text placeholder response.
@@ -22,6 +23,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
